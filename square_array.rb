4723a33a0af64373cca67b5439e8026caf65555c
Josue Abraham <josuea@noukod.com>
2018-09-18 18:46:47 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
-  squared_array = []
+  array_to_square = []
   array.each do |num|
-    square = num ** 2
-    squared_array=square.inject
+    squared = num ** 2
+    array_to_square << squared
   end
-  squared_array
+array_to_square
 end
